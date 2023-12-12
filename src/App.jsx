f5b9324e355510ecb9eca69310d6d2d9bf342a2c
Kherkatary <kherkatarysan456@gmail.com>
2023-12-12 15:50:25 +0545

    Test branch: Adding Signup page Example

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { Footer, Navbar } from './Components'
-import { Home, Login } from './Pages'
+import { Home, Login, SignUp } from './Pages'
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
       </Routes>
       <Footer />
     </>
---
+++ src/Pages/SignUp/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Button} from '../../Components'
+
+const SignUp = () => {
+  return (
+    <div>
+    <p>SignUp</p>
+    <Button title="Sign Up"/>
+    </div>
+  )
+}
+
+export default SignUp
