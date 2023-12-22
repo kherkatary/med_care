87bb5e19e86b6574e4c0f7cf54e35d6d2420639d
kherkatary <kherkatarysan456@gmail.com>
2023-12-22 12:10:34 +0530

    Minor chnages

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import './About.scss'
 import { TileDesc } from '../../Components'
+import './About.scss'
 
 const About = () => {
   return (
     <div>
-        <TileDesc name="About Us"/>
+      <TileDesc name="About Us"/>
         <div style={{backgroundImage: `url("https://img.freepik.com/free-photo/abstract-grunge-decorative-relief-navy-blue-stucco-wall-texture-wide-angle-rough-colored-background_1258-28311.jpg?w=1800&t=st=1703188160~exp=1703188760~hmac=c40d0fcdda0d443539490d44a10b6cc9892c8870bcbb6e411a82f19e1a990f31")`}}
          className="quote_div">
             <div className="quote">
