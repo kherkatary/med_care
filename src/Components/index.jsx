6c453981fc9e2561524599abb781ca146d3cbeee
kherkatary <97865752+kherkatary@users.noreply.github.com>
2023-12-26 13:59:02 +0530

    Revert "ADD: Loader in starting screen"

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -6,16 +6,4 @@ import ContactUsFooter from "./footerComponents/ContactUsFooter/ContactUsFooter"
 import TileDesc from "./TileDesc/TileDesc";
 import Quote from "./QuoteButton/Quote";
 import AboutInfo from "./AboutInfo/AboutInfo";
-import LoadingScreen from "./LoadingScreen/LoadingScreen";
-
-export {
-    Navbar,
-    Footer,
-    Button,
-    NewsPreview,
-    ContactUsFooter,
-    TileDesc,
-    Quote,
-    AboutInfo,
-    LoadingScreen
-}
+export {Navbar, Footer, Button, NewsPreview, ContactUsFooter, TileDesc, Quote, AboutInfo}
