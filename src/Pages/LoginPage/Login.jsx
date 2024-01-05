27957037f84a3bb014cbbaabd581457875d0497b
Sumit Kumar Sah <sumitedu4@gmail.com>
2024-01-05 22:46:34 +0530

    add images

--- src/Pages/LoginPage/Login.jsx
+++ src/Pages/LoginPage/Login.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import './Login.scss'
 import { Link } from 'react-router-dom'
 import { Button } from '../../Components'
-import TileDesc from '../../Components/TileDesc/TileDesc'
-
 const Login = () => {
   return (
     <div className='login'>
-      <TileDesc name='SERVICES'/>
       <p>Login Page</p>
       <Link to="/"><Button title="Home"/></Link>
     </div>
